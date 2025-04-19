33e9b13c9e12cc0870f08b3ef8dd4bdae6f25d03
tarungarg0 <tarungarg0@Gmail.com>
2025-04-20 00:45:08 +0530

    invoice.py

--- invoice.py
+++ invoice.py
@@ -148,8 +148,8 @@ if template_file or default_wb:
         html_filled = html_template
         html_filled = html_filled.replace("{{document_type}}", doc_type)
         html_filled = html_filled.replace("{{recipient_name}}", client_name)
-        html_filled = html_filled.replace("{{delivery_address}}", delivery_address.replace('
-', '<br>'))))
+        html_filled = html_filled.replace("{{delivery_address}}", delivery_address.replace("
+", "<br>")))))
         html_filled = html_filled.replace("{{invoice_date}}", invoice_date.strftime('%d-%m-%Y'))
 
         if items:

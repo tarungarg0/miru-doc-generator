2fa92fb834745059dcdcd6316c110850b350d3a4
tarungarg0 <tarungarg0@Gmail.com>
2025-05-12 21:46:43 +0530

    invoice.py

--- invoice.py
+++ invoice.py
@@ -136,7 +136,7 @@ td {{ border: 1px solid #ccc; font-size: 12px; }}
             th, td {{ padding: 10px; text-align: left; }}
             .total-section {{ display: flex; justify-content: flex-end; margin-top: 20px; }}
             .total-table {{ width: 50%; border-collapse: collapse; }}
-            .total-table th, .total-table td { border: 1px solid #ccc; padding: 10px; text-align: right; font-size: 11px; }}
+            .total-table th, .total-table td { border: 1px solid #ccc; padding: 10px; text-align: right; font-size: 11px; }
             .terms {{ font-size: 0.9em; }}
         </style>
     </head>
